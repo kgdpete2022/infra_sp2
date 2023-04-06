5a60a3dc51fd816153acfc636569426b5474faa0
Пётр Шопин <mail4dev2022@gmail.com>
2023-04-06 12:04:46 +0200

    Updated - DEBUG moved to .env

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR.parent, 'infra/.env'))
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
-DEBUG = int(os.getenv('DEBUG'), False)
+DEBUG = int(os.getenv('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
 
